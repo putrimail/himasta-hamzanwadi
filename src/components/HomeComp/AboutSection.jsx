a0e3eead40b41c1ae29a281c2294c26710c6e95f
putrimail <syarowiputri@gmail.com>
2025-02-06 19:36:07 +0800

    push again

--- src/components/HomeComp/AboutSection.jsx
+++ src/components/HomeComp/AboutSection.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
+const textMisi = [
+  "Meningkatkan kualitas SDM organisasi himasta.",
+  "Menjadikan HIMASTAsebagai wadah untuk mahasiswa menyalurkan aspirasi dan berpartisipasi dalam berbagai kegiatan.",
+  "Menjadikan mahasiswa anggota himasta menjadi mahasiswa yang berprestasi dalam bidang statistika.",
+  "Sebagai sarana dan wadah mahasiswa mengasah potensi dan mengembangkan diri. ",
+  "Menumbuhkan kepedulian mahasiswa terhadap isu-isu dalam kampus maupun di luar kampus.",
+];
 function AboutSection() {
+  const [showVisi, setShowVisi] = useState(true);
+  const [showMisi, setShowMisi] = useState(false);
+  const handleVisi = () => {
+    setShowVisi(!showVisi);
+    setShowMisi(false);
+  };
+  const handleMisi = () => {
+    setShowMisi(!showMisi);
+    setShowVisi(false);
+  };
+
   return (
     <section id="about">
       <div className="bg-red-800 w-full h-auto p-4">
@@ -7,7 +27,7 @@ function AboutSection() {
             <h2 className="text-center font-bold text-4xl  text-white pt-5 ">
               About Us
             </h2>
-            <div className="text-white text-lg py-15 text-justify lg:text-xl font-medium text-pretty">
+            <div className="text-white text-lg p-5 text-justify lg:text-xl font-medium text-pretty">
               <p>
                 HIMASTA (Himpunan Mahasiswa Statistika) Universitas Hamzanwadi
                 adalah organisasi kemahasiswaan yang mewadahi mahasiswa program
@@ -22,6 +42,40 @@ function AboutSection() {
                 lainnya yang mendukung pengembangan keilmuan di bidang
                 statistika dan aplikasinya.
               </p>
+            </div>
+            <div className="text-white text-lg py-5 text-justify lg:text-xl font-medium text-pretty">
+              <div className=" text-black p-3">
+                <div className="bg-white m-3 p-5 ">
+                  <div className="flex justify-between">
+                    <p className="text-2xl font-bold">Visi</p>
+                    <p className="cursor-pointer" onClick={handleVisi}>
+                      {showVisi ? <FaAngleUp /> : <FaAngleDown />}
+                    </p>
+                  </div>
+                  {showVisi && (
+                    <p className="pt-3">
+                      Mewujudkan HIMASTA yang berkualitas yang berporoskan pada
+                      tiga landasan (berproses, berprogres, dan berintegritas)
+                      agar tercapainya cita-cita HIMASTA untuk menjadi yang
+                      lebih baik
+                    </p>
+                  )}
+                </div>
+                <div className="bg-white m-3 p-5 ">
+                  <div className="flex justify-between">
+                    <p className="text-2xl font-bold">Misi</p>
+                    <p className="cursor-pointer" onClick={handleMisi}>
+                      {showMisi ? <FaAngleUp /> : <FaAngleDown />}
+                    </p>
+                  </div>
+                  {showMisi &&
+                    textMisi.map((item, index) => (
+                      <p key={index}>
+                        {index + 1}. {item}
+                      </p>
+                    ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/components/HomeComp/ProfileSeciton.jsx
+++ src/components/HomeComp/ProfileSeciton.jsx
@@ -1,11 +1,12 @@
 import ReactPlayer from "react-player";
+import { Link } from "react-router-dom";
 export default function ProfileSeciton() {
   return (
     <section id="profile">
       <div className="bg-white w-full h-auto p-4">
         <div className="flex-none lg:flex justify-center ">
-          <div className="lg:w-1/2   ">
-            <h2 className="text-4xl font-bold mb-4 text-red-800 text-center pt-5">
+          <div className="lg:w-1/2 text-center  ">
+            <h2 className="text-4xl font-bold mb-4 text-red-800  pt-5">
               Our Profile
             </h2>
             <div className="w-full max-w-3xl py-15 ">
@@ -16,6 +17,12 @@ export default function ProfileSeciton() {
                 controls={true}
               />
             </div>
+            <Link
+              to="https://www.youtube.com/@himastahamzanwadi4014"
+              className="bg-red-800 p-3 rounded-lg cursor-pointer text-white text-lg"
+            >
+              video lainnya....
+            </Link>
           </div>
         </div>
       </div>
